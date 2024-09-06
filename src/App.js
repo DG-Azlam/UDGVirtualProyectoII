51bbc8b95aa4113366c6ead9227f358784b16765
DG-Azlam <159468036+DG-Azlam@users.noreply.github.com>
2024-09-05 23:36:13 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import ReactDOM from 'react-dom/client';
 
 function App() {
+  const [name,setName] = useState("");
+  const [pass,setPass] = useState("");
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <form>
+          <label> usuario: {"\n"}
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+          
+          <label> contraseña: {"\n"}
+            <input type="text" value={pass} onChange={(e) => setPass(e.target.value)}/>
+          </label>
+        </form>
       </header>
     </div>
   );
 }
+
+const root= ReactDOM.createRoot(document.getElementById('root'));
+root.render(<login />)
 
 export default App;
